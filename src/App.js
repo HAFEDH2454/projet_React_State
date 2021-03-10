754c7a85906aa39b8c685047eead232a2bc9d8da
HAFEDH2454 <71188499+HAFEDH2454@users.noreply.github.com>
2021-03-10 06:46:26 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+  constructor(props) 
+  {
+    super(props);
+    this.state =  {fullName:"hafedh", bio:"my_bio",
+     imgSrc:"my_img", date: new Date(), profession:"my_profession", abs:true} ;
+     this.timer=null;
+  }
+  componentDidMount (){
+    this.timer= window.setInterval(() => {
+      this.setState({
+        date:new Date()
+      })
+    })
+  }
+  componentWillUnmount(){
+    window.clearInterval(this.timer)
+  }
+  changeabs = () => {
+   if(this.state.abs==false)
+      { {this.state.abs=false};}
+      else
+      {{this.state.abs=true};}
+  }
+  
+  render() {
+    return (
+      <div style={{margin:"20%"}}>
+          {this.state.date.toLocaleString()}
+          if ({this.state.abs})
+              {<h1>my profile {this.state.bio}</h1>}
+              
+        <button
+          type="button"
+          onClick={this.changeabs}
+        >affiche {this.changeabs}</button>
+      
+      </div>
+    );
+  }
+
 }
 
 export default App;
